05408dff120497576cfa93210e93204c6fa09d17
Adefemi Kolawole <akolawole@ggc.edu>
2019-06-09 21:56:43 -0400

    wrote some lines into the Student clas, but the main db implementation is in getStudents

--- src/Student.java
+++ src/Student.java
@@ -1,2 +1,65 @@
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class Student {
+    private int id;
+    private String first_name;
+    private String last_name;
+    private String dob;
+    private String  classes;
+    private String  email;
+
+    public Student(){
+
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public String getFirst_name() {
+        return this.first_name;
+    }
+
+    public String getLast_name() {
+        return this.last_name;
+    }
+
+    public String getDob() {
+        return this.dob;
+    }
+
+    public String getClasses() {
+        return this.classes;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public static void getStudents(ResultSet resultSet) throws SQLException {
+        while (resultSet.next()){
+            StringBuffer buffer = new StringBuffer();
+            buffer.append("Student{" +
+                    "id=" + resultSet.getInt("id") +
+                    ", first_name='" + resultSet.getString("first_name")+ '\'' +
+                    ", last_name='" + resultSet.getString("last_name") + '\'' +
+                    ", dob='" + resultSet.getString("dob") + '\'' +
+                    ", classes='" + resultSet.getInt("class") + '\'' +
+                    ", email='" + resultSet.getString("email") + '\'' +
+                    '}');
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", first_name='" + first_name + '\'' +
+                ", last_name='" + last_name + '\'' +
+                ", dob='" + dob + '\'' +
+                ", classes='" + classes + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
 }
--- src/TestRunner.java
+++ src/TestRunner.java
@@ -2,13 +2,15 @@ import java.sql.SQLException;
 
 public class TestRunner {
     public static void main(String[] args) {
+        Student student = new Student();
+        System.out.println(student.toString());
         //System.out.println("My name is Adefemi Kolawole. I am a Java developer");
         System.out.println("**Connecting to database**..........");
         try {
            // DBConnection.connect();
-            DBConnection2.connectToTable("students");
+            DBConnection.connectToTable("students");
             System.out.println("************************************");
-            DBConnection2.connectToTable("teacher");
+            DBConnection.connectToTable("teacher");
         } catch (SQLException e) {
            // e.printStackTrace();
         }
